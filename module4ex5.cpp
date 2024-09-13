83f70f0a3df0b1a12db8c414fd27d94a6c71018f
MyliuH <138070641+MyliuH@users.noreply.github.com>
2024-09-14 02:11:32 +0300

    Add files via upload

---
+++ module4ex5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> vecplusn(vector<int> mus, int n) {
+    for (int i = 0; i < mus.size(); i++) {
+        mus[i] += n;
+    }
+    return mus;
+}
+
+int main()
+{
+    vector<int> mus = { 1, 2, 3, 4, 5 };
+    vector<int> musresult;
+    int n;
+    cin >> n;
+    musresult = vecplusn(mus, n);
+    for (auto i : musresult) {
+        cout << i << " ";
+    }
+}
+
